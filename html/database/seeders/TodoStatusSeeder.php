0911e78cb282ae16d2cdbf1820b4ab44bd7036f9
Jeymsson Nogueira <jeymsson@gmail.com>
2023-05-19 14:54:18 -0300

    feat: add model, migrate, seed, controller, test
    Commands bellow:
    php artisan make:model Todo -m
    php artisan make:controller TodoController -r
    php artisan make:seeder TodoSeeder
    php artisan make:test TodoTest

---
+++ html/database/seeders/TodoStatusSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TodoStatusSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+
+}
